573de54cae71b258fddbd6338c7f81ec952d72dd
James Stevenson <james.l.stevenson@gmail.com>
2020-12-13 22:33:28 +0000

    Added significant changes in tooling up to the stage of feature extraction. Features not implemented include follower/ following frequency and number of post frequency as the data set being used does not support these features.

---
+++ Grapher.py
@@ -0,0 +1,52 @@
+import networkx as nx
+
+class grapher():
+    """
+    A wrapper class used for generating a graph for interactions between users
+    """
+    graph = None
+
+    def __init__(self):
+        """
+        Constructor.
+        """
+        self.graph = nx.DiGraph()
+
+    def add_edge_wrapper(self, node_1_name, node_2_name, weight, relationship):
+        """
+        A wrapper function used to add an edge connection or node.
+        :param node_1_name: from
+        :param node_2_name: to
+        :param weight:
+        :param relationship:
+        :return:
+        """
+        self.graph.add_edge(node_1_name,node_2_name,weight=weight,relation =relationship)
+
+    def get_info(self):
+        """
+        Retrieves information about the graph
+        :return:
+        """
+        return nx.info(self.graph)
+
+    def show_graph(self):
+        """
+        Displays the graph
+        :return:
+        """
+        nx.spring_layout(self.graph)
+
+    def get_degree_centrality_for_user(self, user_name):
+        """
+        Returns the Degree of Centrality for a given user present in the graph
+        :param user_name:
+        :return: the Degree of Centrality for a given user present in the graph
+        """
+        centrality = nx.degree_centrality(self.graph)
+        return centrality[user_name]
+
+    # todo implement
+    #def get_eigenvector_centrality_for_user(self, user_name):
+    #    centrality = nx.eigenvector_centrality(self.graph)
+    #    return centrality[user_name]
