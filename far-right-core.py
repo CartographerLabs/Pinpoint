25e8463705fe8f67f97fd669ca6e34a141af74e0
James Stevenson <28961606+user1342@users.noreply.github.com>
2021-05-08 14:15:17 +0100

    Changing core so that it processesses all permutations of feature extraction

--- far-right-core.py
+++ far-right-core.py
@@ -23,10 +23,41 @@ model.BEHAVIOURAL_FEATURES_ENABLED = True
 model.PSYCHOLOGICAL_SIGNALS_ENABLED = True
 
 model.train_model(features_file= r"outputs/training_features.json",
-                  force_new_dataset=False, model_location=r"outputs/far-right-baseline.model")  # , model_location=r"Pinpoint/model/my.model"
+                  force_new_dataset=True, model_location=r"outputs/far-right-baseline.model")  # , model_location=r"Pinpoint/model/my.model"
 
 model.create_model_info_output_file(location_of_output_file="outputs/far-right-baseline-output.txt",
                                     training_data_csv_location=r"outputs/training_features.json.csv")
+
+model.RADICAL_LANGUAGE_ENABLED = True
+model.BEHAVIOURAL_FEATURES_ENABLED = False
+model.PSYCHOLOGICAL_SIGNALS_ENABLED = False
+
+model.train_model(features_file= r"C:\Projects\Pinpoint\outputs\training_features.json",
+                  force_new_dataset=True, model_location=r"outputs\radical-language.model")  # , model_location=r"Pinpoint/model/my.model"
+
+model.create_model_info_output_file(location_of_output_file="outputs/radical-language-output.txt",
+                                    training_data_csv_location=r"C:\Projects\Pinpoint\outputs\training_features.json.csv")
+
+model.RADICAL_LANGUAGE_ENABLED = False
+model.BEHAVIOURAL_FEATURES_ENABLED = True
+model.PSYCHOLOGICAL_SIGNALS_ENABLED = False
+
+model.train_model(features_file= r"C:\Projects\Pinpoint\outputs\training_features.json",
+                  force_new_dataset=True, model_location=r"outputs\behavioural.model")  # , model_location=r"Pinpoint/model/my.model"
+
+model.create_model_info_output_file(location_of_output_file="outputs/behavioural-output.txt",
+                                    training_data_csv_location=r"C:\Projects\Pinpoint\outputs\training_features.json.csv")
+
+
+model.RADICAL_LANGUAGE_ENABLED = False
+model.BEHAVIOURAL_FEATURES_ENABLED = False
+model.PSYCHOLOGICAL_SIGNALS_ENABLED = True
+
+model.train_model(features_file= r"C:\Projects\Pinpoint\outputs\training_features.json",
+                  force_new_dataset=True, model_location=r"outputs\psychological.model")  # , model_location=r"Pinpoint/model/my.model"
+
+model.create_model_info_output_file(location_of_output_file="outputs/psychological-output.txt",
+                                    training_data_csv_location=r"C:\Projects\Pinpoint\outputs\training_features.json.csv")
 
 
 print("Finished")
