4f5827e3229b9952d1ea758939af634ad77cc59b
James Stevenson <28961606+user1342@users.noreply.github.com>
2021-05-09 08:03:28 +0100

    Adding check centrality and count hashtags script

--- check-centrality.py
+++ check-centrality.py
@@ -2,7 +2,7 @@ import json
 
 messages_from_file = []
 
-with open(r'C:\Projects\Pinpoint\outputs\training_features.json') as json_file:
+with open(r'outputs/training_features.json') as json_file:
     data = json.load(json_file)
     for message_entity in data:
         messages_from_file.append(message_entity)
@@ -18,7 +18,7 @@ for message_entity in messages_from_file:
         centrality = message["centrality"]
         is_extreamist = str(message["is_extremist"]).lower()
 
-        if centrality != 0:
+        if centrality > 0:
             if is_extreamist in comparison_dict.keys():
                 comparison_dict[is_extreamist] = comparison_dict[is_extreamist] + 1
             else:
@@ -30,9 +30,11 @@ total_not_extreamist = comparison_dict["false"]
 total = total_is_extreamist + total_not_extreamist
 
 is_extreamist_percentage = (total_is_extreamist/ total) * 100
-not_extreamist_percentage = (total_not_extreamist/total) * 100
+not_extreamist_percentage = (total_not_extreamist/total) *100
 
-print("Of the posts with a centrality higher than 0, {}% were extreamist posts and {}% were non-extreamist posts")
+print("Of the '{}' posts with a centrality higher than 0, '{}%' were extreamist posts and '{}%' were non-extreamist posts".format(
+    total,is_extreamist_percentage, not_extreamist_percentage
+))
 
-output_file = open("centrality-count.txt", "w", encoding="utf8")
-json.dump(comparison_dict, output_file)
+with open("centrality-count.txt", 'w', encoding="utf8") as outfile:
+    json.dump(comparison_dict, outfile)
