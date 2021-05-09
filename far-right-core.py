574d15557ea5214ac40e3a46ab328b1333b5a6c7
James Stevenson <28961606+user1342@users.noreply.github.com>
2021-05-09 06:50:32 +0100

    Changing far-right-core so that it processesses all permutations of feature extraction

--- far-right-core.py
+++ far-right-core.py
@@ -32,32 +32,33 @@ model.RADICAL_LANGUAGE_ENABLED = True
 model.BEHAVIOURAL_FEATURES_ENABLED = False
 model.PSYCHOLOGICAL_SIGNALS_ENABLED = False
 
-model.train_model(features_file= r"C:\Projects\Pinpoint\outputs\training_features.json",
-                  force_new_dataset=True, model_location=r"outputs\radical-language.model")  # , model_location=r"Pinpoint/model/my.model"
+model.train_model(features_file= r"outputs/training_features.json",
+                  force_new_dataset=True, model_location=r"outputs/far-right-radical-language.model")  # , model_location=r"Pinpoint/model/my.model"
 
-model.create_model_info_output_file(location_of_output_file="outputs/radical-language-output.txt",
-                                    training_data_csv_location=r"C:\Projects\Pinpoint\outputs\training_features.json.csv")
+model.create_model_info_output_file(location_of_output_file="outputs/far-right-radical-language-output.txt",
+                                    training_data_csv_location=r"outputs/training_features.json.csv")
+
 
 model.RADICAL_LANGUAGE_ENABLED = False
 model.BEHAVIOURAL_FEATURES_ENABLED = True
 model.PSYCHOLOGICAL_SIGNALS_ENABLED = False
 
-model.train_model(features_file= r"C:\Projects\Pinpoint\outputs\training_features.json",
-                  force_new_dataset=True, model_location=r"outputs\behavioural.model")  # , model_location=r"Pinpoint/model/my.model"
+model.train_model(features_file= r"outputs/training_features.json",
+                  force_new_dataset=True, model_location=r"outputs/far-right-behavioural.model")  # , model_location=r"Pinpoint/model/my.model"
 
-model.create_model_info_output_file(location_of_output_file="outputs/behavioural-output.txt",
-                                    training_data_csv_location=r"C:\Projects\Pinpoint\outputs\training_features.json.csv")
+model.create_model_info_output_file(location_of_output_file="outputs/far-right-behavioural-output.txt",
+                                    training_data_csv_location=r"outputs/training_features.json.csv")
 
 
 model.RADICAL_LANGUAGE_ENABLED = False
 model.BEHAVIOURAL_FEATURES_ENABLED = False
 model.PSYCHOLOGICAL_SIGNALS_ENABLED = True
 
-model.train_model(features_file= r"C:\Projects\Pinpoint\outputs\training_features.json",
-                  force_new_dataset=True, model_location=r"outputs\psychological.model")  # , model_location=r"Pinpoint/model/my.model"
+model.train_model(features_file= r"outputs/training_features.json",
+                  force_new_dataset=True, model_location=r"outputs/far-right-psychological.model")  # , model_location=r"Pinpoint/model/my.model"
 
-model.create_model_info_output_file(location_of_output_file="outputs/psychological-output.txt",
-                                    training_data_csv_location=r"C:\Projects\Pinpoint\outputs\training_features.json.csv")
+model.create_model_info_output_file(location_of_output_file="outputs/far-right-psychological-output.txt",
+                                    training_data_csv_location=r"outputs/training_features.json.csv")
 
 
 print("Finished")
