4bc5a725485c62e4a11ea1b3326add823518accf
James Stevenson <28961606+user1342@users.noreply.github.com>
2021-05-09 09:09:16 +0100

    Adding check centrality and count hashtags script

--- count-hashtags.py
+++ count-hashtags.py
@@ -32,38 +32,35 @@ with open(clean_message_csv_location, newline='', encoding="cp1252") as csvfile:
         length_of_file = length_of_file + 1
         messages.append(row[1])
 
-# Loops for a max depth
-for depth in range(0, depth_range):
-    collected_hashtags = {}
 
-    with open(clean_message_csv_location, newline='', encoding="utf8") as csvfile:
-        search_hastag_count = 0
+with open(clean_message_csv_location, newline='', encoding="utf8") as csvfile:
+    search_hastag_count = 0
 
-        # Loops through all hastags to be searched for
-        for search_hashtag in list_of_hashtags:
+    # Loops through all hastags to be searched for
+    for search_hashtag in list_of_hashtags:
 
-            # Write to file as we go
-            sorted(dict_of_hashtag_count, key=dict_of_hashtag_count.get, reverse=True)
-            output_file = open("hashtag-count.txt", "w", encoding="utf8")
-            json.dump(dict_of_hashtag_count, output_file)
-            output_file.close()
+        # Write to file as we go
+        sorted(dict_of_hashtag_count, key=dict_of_hashtag_count.get, reverse=True)
+        output_file = open("hashtag-count.txt", "w", encoding="utf8")
+        json.dump(dict_of_hashtag_count, output_file)
+        output_file.close()
 
-            search_hastag_count = search_hastag_count+ 1
-            # Loop through all rows of messages looking for the hastag that's being searched for
-            message_count = 0
+        search_hastag_count = search_hastag_count+ 1
+        # Loop through all rows of messages looking for the hastag that's being searched for
+        message_count = 0
 
-            # Loops through all messages
-            for message in messages:
-                message_count = message_count + 1
-                print("Hashtag '{}' of Hashtags '{}', searched in message '{}' of messages '{}'".format(
-                    search_hastag_count, len(list_of_hashtags), message_count, len(messages)))
+        # Loops through all messages
+        for message in messages:
+            message_count = message_count + 1
+            print("Hashtag '{}' of Hashtags '{}', searched in message '{}' of messages '{}'".format(
+                search_hastag_count, len(list_of_hashtags), message_count, len(messages)))
 
-                if search_hashtag.lower() in message.lower():
+            if search_hashtag.lower() in message.lower():
 
-                    if search_hashtag.lower() in dict_of_hashtag_count.keys():
-                        dict_of_hashtag_count[search_hashtag.lower()] = dict_of_hashtag_count[search_hashtag.lower()] + 1
-                    else:
-                        dict_of_hashtag_count[search_hashtag.lower()] = 1
+                if search_hashtag.lower() in dict_of_hashtag_count.keys():
+                    dict_of_hashtag_count[search_hashtag.lower()] = dict_of_hashtag_count[search_hashtag.lower()] + 1
+                else:
+                    dict_of_hashtag_count[search_hashtag.lower()] = 1
 
 # Write to file when completed
 sorted(dict_of_hashtag_count, key=dict_of_hashtag_count.get, reverse=True)
